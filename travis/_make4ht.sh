784fe044d5b00dd0e0f788500e317178cc17a996
Sergey Kojoian <sergei.kojoian@gmail.com>
2021-06-20 22:25:07 -0700

    Revert local make4ht repo

--- travis/_make4ht.sh
+++ travis/_make4ht.sh
@@ -2,8 +2,15 @@
 
 set -e
 
-cp external/make4ht-3d97d23 make4ht-3d97d23
-cd make4ht-3d97d23
+[ -z "${GITHUB_PAT}" ] && exit 0
+
+git config --global user.email "Overseer.O5.X@gmail.com"
+git config --global user.name "Fifthist"
+
+git clone https://github.com/michal-h21/make4ht.git make4ht-latest
+cp external/make4ht/make4ht-doc.tex make4ht-latest
+cd make4ht-latest
+git checkout 3d97d23
 make
 make install
 cd ../
